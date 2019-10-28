656aec96cd6a89f4b75a0bf274eab52019f30c8a
Josiah Hollibaugh <jo.hollibaugh@gmail.com>
2019-10-28 16:59:27 -0500

    Canvas Works

--- MindOverMatter/wwwRoot/Home/CanvasTest.js
+++ MindOverMatter/wwwRoot/Home/CanvasTest.js
@@ -1,9 +1,34 @@
-﻿var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [100, -50]);
+﻿// Lots O' Circles
+//function onMouseDrag(event) {
+//    var radius = event.delta.length / 2;
+//    var circle = new Path.Circle(event.middlePoint, radius);
+//    circle.fillColor = 'black';
+//}
+var Molecule = new Array();
+var CList = new Array();
+var path;
+var circle;
+var prevCircle;
+function onMouseDown(event) {
+    path = new Path();
+    path.strokeColor = 'black';
+    circle = new Path.Circle(event.downPoint, 5);
+    circle.fillColor = 'black';
+    CList.push(circle);
+    path.add(event.downPoint, event.lastPoint);
+    //if (typeof prevCircle !== 'undefined') {
+    //    var shortest;
+    //    var point;
+    //    for (var i = 0; i < CList.length; i++) {
+
+    //        Tshortest = CList[i].bounds.centerX + CList[i].bounds.centerY;
+    //        if (typeof shortest == 'undefined' || Tshortest < shortest) {
+    //            shortest = Tshortest;
+    //            point = { x: CList[i].bounds.centerX, y: CList[i].bounds.centerY };
+    //        }
+    //    }
+    //    path.add(new Path.Line(event.downPoint, new Point(point.x,point.y)));
+    //    Cist.add('b');
+    //}
+    //prevCircle = circle;
+}
