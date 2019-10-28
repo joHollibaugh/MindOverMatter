20c34b775b292a4bff121415a40ff9fe6d1db84c
joHollibaugh <jo.hollibaugh@gmail.com>
2019-10-28 18:47:24 -0500

    Improved Canvas

--- MindOverMatter/wwwRoot/Home/CanvasTest.js
+++ MindOverMatter/wwwRoot/Home/CanvasTest.js
@@ -5,7 +5,7 @@
 //    circle.fillColor = 'black';
 //}
 var Molecule = new Array();
-var CList = new Array();
+var PointList = new Array();
 var path;
 var circle;
 var prevCircle;
@@ -14,21 +14,20 @@ function onMouseDown(event) {
     path.strokeColor = 'black';
     circle = new Path.Circle(event.downPoint, 5);
     circle.fillColor = 'black';
-    CList.push(circle);
-    path.add(event.downPoint, event.lastPoint);
-    //if (typeof prevCircle !== 'undefined') {
-    //    var shortest;
-    //    var point;
-    //    for (var i = 0; i < CList.length; i++) {
-
-    //        Tshortest = CList[i].bounds.centerX + CList[i].bounds.centerY;
-    //        if (typeof shortest == 'undefined' || Tshortest < shortest) {
-    //            shortest = Tshortest;
-    //            point = { x: CList[i].bounds.centerX, y: CList[i].bounds.centerY };
-    //        }
-    //    }
-    //    path.add(new Path.Line(event.downPoint, new Point(point.x,point.y)));
-    //    Cist.add('b');
-    //}
-    //prevCircle = circle;
+    if (typeof prevCircle !== 'undefined') {
+        var shortest;
+        var distance;
+        var point;
+        for (var i = 0; i < PointList.length; i++) {
+            
+            distance = PointList[i].getDistance(event.downPoint);
+            if (typeof shortest == 'undefined' || distance < shortest) {
+                shortest = distance;
+                point = PointList[i];
+            }
+        }
+        path.add(event.downPoint, point);
+    }
+    PointList.push(event.downPoint);
+    prevCircle = circle;
 }
