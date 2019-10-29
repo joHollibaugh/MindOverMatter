98845cd06be902f868d899d8fe518fffc2cf9084
joHollibaugh <jo.hollibaugh@gmail.com>
2019-10-28 19:20:09 -0500

    Working on Canvas

--- MindOverMatter/wwwRoot/Home/CanvasTest.js
+++ MindOverMatter/wwwRoot/Home/CanvasTest.js
@@ -10,24 +10,46 @@ var path;
 var circle;
 var prevCircle;
 function onMouseDown(event) {
+    debugger;
     path = new Path();
     path.strokeColor = 'black';
-    circle = new Path.Circle(event.downPoint, 5);
-    circle.fillColor = 'black';
     if (typeof prevCircle !== 'undefined') {
         var shortest;
         var distance;
         var point;
+        var CRef;
         for (var i = 0; i < PointList.length; i++) {
-            
             distance = PointList[i].getDistance(event.downPoint);
-            if (typeof shortest == 'undefined' || distance < shortest) {
-                shortest = distance;
-                point = PointList[i];
+            if (distance <= 50) {
+                if (typeof shortest == 'undefined' || distance < shortest) {
+                    shortest = distance;
+                    point = PointList[i];
+                }
             }
         }
-        path.add(event.downPoint, point);
+        if (typeof point !== 'undefined') {
+            path.add(event.downPoint, point);
+            //CRef = Molecule.find(obj => {
+            //    return obj.P === point;
+            //});
+            var C = { b: [CRef.ID], ID: "C" + event.count, P: event.downPoint }
+            Molecule.push(C);
+            CRef.b.push(C.ID);
+            circle = new Path.Circle(event.downPoint, 5);
+            circle.fillColor = 'black';
+            PointList.push(event.downPoint);
+            prevCircle = circle;
+
+        }
     }
-    PointList.push(event.downPoint);
-    prevCircle = circle;
+    else {
+        debugger;
+            circle = new Path.Circle(event.downPoint, 5);
+            circle.fillColor = 'black';
+            prevCircle = circle;
+        PointList.push(event.downPoint);
+        var C = { b: new Array(), ID: "C" + event.count, P: null, Loc: event.downPoint}
+        Molecule.push(C);
+
+    }
 }
