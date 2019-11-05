14473921b82f11c8cd09048c42960831861b3369
Josiah Hollibaugh <jo.hollibaugh@gmail.com>
2019-11-05 10:54:38 -0600

    VIsual Changes

--- MindOverMatter/wwwRoot/Home/CanvasTest.js
+++ MindOverMatter/wwwRoot/Home/CanvasTest.js
@@ -1,8 +1,23 @@
-﻿// Lots O' Circles
+﻿// //Lots O' Circles
+//var i = 1;
 //function onMouseDrag(event) {
 //    var radius = event.delta.length / 2;
 //    var circle = new Path.Circle(event.middlePoint, radius);
-//    circle.fillColor = 'black';
+
+    //if (i % 2 === 0) {
+    //    circle.fillColor = "#f64f59";
+    //}
+    //else if (i % 3 === 0) {
+    //    circle.fillColor = '#c471ed';
+
+    //}
+    //else {
+    //    circle.fillColor = "#12c2e9";
+    //}
+    //i+= 1;
+    
+
+//    circle.fillColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 //}
 var Molecule = new Array();
 var PointList = new Array();
@@ -26,7 +41,7 @@ function getName(Mol) {
 
 function onMouseDown(event) {
     path = new Path();
-    path.strokeColor = 'black';
+    path.strokeColor = '#00ff00';
     if (typeof prevCircle !== 'undefined') {
         var shortest;
         var distance;
@@ -34,7 +49,7 @@ function onMouseDown(event) {
         var CRef;
         for (var i = 0; i < PointList.length; i++) {
             distance = PointList[i].getDistance(event.downPoint);
-            if (distance <= 50) {
+            if (distance <= 75) {
                 if (typeof shortest == 'undefined' || distance < shortest) {
                     shortest = distance;
                     point = PointList[i];
@@ -52,15 +67,15 @@ function onMouseDown(event) {
             var C = { b: [CRef.ID], ID: "C" + event.count, P: null, Loc: event.downPoint }
             Molecule.push(C);
             CRef.b.push(C.ID);
-            circle = new Path.Circle(event.downPoint, 5);
-            circle.fillColor = 'black';
+            circle = new Path.Circle(event.downPoint, 7);
+            circle.fillColor = '#00ff00';
             PointList.push(event.downPoint);
             prevCircle = circle;
         }
     }
     else {
-        circle = new Path.Circle(event.downPoint, 5);
-        circle.fillColor = 'black';
+        circle = new Path.Circle(event.downPoint, 7);
+        circle.fillColor = '#00ff00';
         prevCircle = circle;
         PointList.push(event.downPoint);
         var C = { b: new Array(), ID: "C" + event.count, P: null, Loc: event.downPoint };
