566b0c4476e41d910e05beb794dd0682482ffea9
es555691 <es555691@LR00212>
2019-11-11 18:56:01 -0600

    Fixed the logic, hopefully..

--- MindOverMatter/wwwRoot/Home/CanvasTest.js
+++ MindOverMatter/wwwRoot/Home/CanvasTest.js
@@ -33,7 +33,7 @@ function getName(Mol) {
     Mol.Neighbors = Mol.b;
     Mol.NodeID = Mol.ID;
 
-    JSONPost("/Chemical/getMolecule", { input: JSON.stringify(Mol) } , successCallback);
+    JSONPost("/Chemical/GetMoleculeName", {/* input: JSON.stringify(Mol) */} , successCallback);
     function successCallback(response) {
         var data = response.d;
         alert(data);
