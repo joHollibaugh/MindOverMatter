dc44bcba3120a7981c1cee55754866de84650fcc
es555691 <es555691@LR00212>
2019-11-18 19:07:32 -0600

    Added prefixes to the db

--- MindOverMatter/Migrations/20191119004031_AddPrefixes.Designer.cs
+++ MindOverMatter/Migrations/20191119004031_AddPrefixes.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MindOverMatter.Models.DbContexts;
 
 namespace MindOverMatter.Migrations
 {
     [DbContext(typeof(ChemicalDbContext))]
-    partial class ChemicalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191119004031_AddPrefixes")]
+    partial class AddPrefixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -67,29 +69,6 @@ namespace MindOverMatter.Migrations
                     b.HasKey("PrefixId");
 
                     b.ToTable("Prefixes");
-
-                    b.HasData(
-                        new { PrefixId = 1, ChainLength = 1, Name = "Meth" },
-                        new { PrefixId = 2, ChainLength = 2, Name = "Ethane" },
-                        new { PrefixId = 3, ChainLength = 3, Name = "Prop" },
-                        new { PrefixId = 4, ChainLength = 4, Name = "But" },
-                        new { PrefixId = 5, ChainLength = 5, Name = "Pent" },
-                        new { PrefixId = 6, ChainLength = 6, Name = "Hex" },
-                        new { PrefixId = 7, ChainLength = 7, Name = "Hept" },
-                        new { PrefixId = 8, ChainLength = 8, Name = "Oct" },
-                        new { PrefixId = 9, ChainLength = 9, Name = "Non" },
-                        new { PrefixId = 10, ChainLength = 10, Name = "Dec" },
-                        new { PrefixId = 11, ChainLength = 11, Name = "Undec" },
-                        new { PrefixId = 12, ChainLength = 12, Name = "Dodec" },
-                        new { PrefixId = 13, ChainLength = 13, Name = "Tridec" },
-                        new { PrefixId = 14, ChainLength = 14, Name = "Tetradec" },
-                        new { PrefixId = 15, ChainLength = 15, Name = "Pentadec" },
-                        new { PrefixId = 16, ChainLength = 16, Name = "Hexadec" },
-                        new { PrefixId = 17, ChainLength = 17, Name = "Heptadec" },
-                        new { PrefixId = 18, ChainLength = 18, Name = "Octadec" },
-                        new { PrefixId = 19, ChainLength = 19, Name = "Nonadec" },
-                        new { PrefixId = 20, ChainLength = 20, Name = "Icos" }
-                    );
                 });
 #pragma warning restore 612, 618
         }
---
+++ MindOverMatter/Migrations/20191119005718_AddPrefixesSeedData.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MindOverMatter.Migrations
+{
+    public partial class AddPrefixesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Prefixes",
+                columns: new[] { "PrefixId", "ChainLength", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Meth" },
+                    { 18, 18, "Octadec" },
+                    { 17, 17, "Heptadec" },
+                    { 16, 16, "Hexadec" },
+                    { 15, 15, "Pentadec" },
+                    { 14, 14, "Tetradec" },
+                    { 13, 13, "Tridec" },
+                    { 12, 12, "Dodec" },
+                    { 11, 11, "Undec" },
+                    { 10, 10, "Dec" },
+                    { 9, 9, "Non" },
+                    { 8, 8, "Oct" },
+                    { 7, 7, "Hept" },
+                    { 6, 6, "Hex" },
+                    { 5, 5, "Pent" },
+                    { 4, 4, "But" },
+                    { 3, 3, "Prop" },
+                    { 2, 2, "Ethane" },
+                    { 19, 19, "Nonadec" },
+                    { 20, 20, "Icos" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Prefixes",
+                keyColumn: "PrefixId",
+                keyValue: 20);
+        }
+    }
+}
